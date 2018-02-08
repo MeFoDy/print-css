2c5fd4ea517d8514dff2c5f2663cbe057e052a8a
Nikita Dubko <mefody93@gmail.com>
2018-02-08 19:40:25 +0300

    Add Math formulas generation

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,39 @@
 const
+    fs = require('fs'),
     gulp = require('gulp'),
     run = require('gulp-run'),
+    mjpage = require('mathjax-node-page'),
+    mjnode = require('mathjax-node-svg2png'),
     browserSync = require('browser-sync');
 
-gulp.task('build', function() {
+gulp.task('build', ['mathjax'], function () {
     return run('npm run build')
         .exec();
 });
 
-gulp.task('pdf', function() {
+gulp.task('pdf', function () {
     return run('npm run pdf')
         .exec();
-})
+});
 
-gulp.task('serve', ['build'], function() {
+gulp.task('md', function () {
+    return run('npm run md')
+        .exec();
+});
+
+gulp.task('mathjax', ['md'], function (cb) {
+    mjpage.init(mjnode);
+    mjpage.addOutput('png', (wrapper, data) => {
+        wrapper.innerHTML = `<img src="${data}">`;
+    });
+    const input = fs.readFileSync('index.html');
+    mjpage.mjpage(input, { format: ["TeX"] }, { png: true, scale: 1.2 }, function (output) {
+        fs.writeFileSync('index.html', output);
+        cb();
+    });
+});
+
+gulp.task('serve', ['build'], function () {
     browserSync.init({
         server: {
             baseDir: "./"
